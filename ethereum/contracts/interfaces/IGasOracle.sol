e546efde92d74498ad5e9d49f6aa06e1220d3064
Karl Kempe <karlkempe@users.noreply.github.com>
2022-09-13 21:51:57 +0000

    ethereum: fix interface

--- ethereum/contracts/interfaces/IGasOracle.sol
+++ ethereum/contracts/interfaces/IGasOracle.sol
@@ -4,8 +4,5 @@
 pragma solidity ^0.8.0;
 
 interface IGasOracle {
-    //Returns the price of one unit of gas on the wormhole targetChain, denominated in this chain's wei.
     function getPrice(uint16 targetChainId) external view returns (uint256 quote);
-
-    function updatePrice(uint16 chainId, uint256 gasPrice, uint256 nativeCurrencyPrice) external;
 }
