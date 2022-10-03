7a75a33d197989bae83a7fe8572061bde2cdc94f
Reptile <43194093+dsterioti@users.noreply.github.com>
2022-10-03 08:30:17 -0500

    Start offchain-relayer process in integration test script (#12)
    Co-authored-by: Drew <dsterioti@users.noreply.github.com>

--- testing/run_tilt_tests.sh
+++ testing/run_tilt_tests.sh
@@ -6,8 +6,15 @@ if [ $? -ne 0 ]; then
     exit 1;
 fi
 
+# start the offchain-relayer process
+echo "Starting off-chain relayer process."
+cd $(dirname $0)/../offchain-relayer && ./start-relayer.sh > offchain-relayer.out 2>&1 &
+
 # go to the sdk directory
 cd $(dirname $0)/../sdk
 
 # run tests in sdk directory
 npm run test
+
+# kill the offchain-relayer process
+pkill -f "exe/main relay"
