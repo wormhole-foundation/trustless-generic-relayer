28548266b5f8b47a8806846223f20aedb0137f57
chase-45 <chasemoran45@gmail.com>
2022-11-19 11:10:13 -0500

    created relayer engine directory

---
+++ relayer_engine/README.md
@@ -0,0 +1,3 @@
+This directory represents the plugin intended to be used with the relayer engine.
+
+The implementation of the plugin is in the /plugin directory, where the main.ts file is intended to start an engine with the plugin loaded.
---
+++ relayer_engine/main.ts
@@ -0,0 +1,22 @@
+import * as relayerEngine from "@wormhole-foundation/relayer-engine";
+import GenericRelayerPluginDef, { GenericRelayerPluginConfig } from "./plugin/src/plugin";
+
+async function main() {
+  // load plugin config
+  const pluginConfig = (await relayerEngine.loadFileAndParseToObject(
+    `./plugin/config/${relayerEngine.EnvType.DEVNET.toLowerCase()}.json`
+  )) as GenericRelayerPluginConfig;
+
+  // run relayer engine
+  await relayerEngine.run({
+    configs: "./engine_config",
+    plugins: [GenericRelayerPluginDef.init(pluginConfig)],
+    mode: relayerEngine.Mode.BOTH,
+  });
+}
+
+// allow main to be an async function and block until it rejects or resolves
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
