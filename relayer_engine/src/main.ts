4d8e08798e359525dc156b7a78048802aa31bd54
Joe Howarth <josephehowarth@gmail.com>
2022-12-29 11:20:09 -0600

    prepare offchain relayer to run in both tilt and testnet

--- relayer_engine/src/main.ts
+++ relayer_engine/src/main.ts
@@ -1,22 +1,38 @@
-import * as relayerEngine from "@wormhole-foundation/relayer-engine";
-import GenericRelayerPluginDef, { GenericRelayerPluginConfig } from "./plugin/src/plugin";
+import * as relayerEngine from "@wormhole-foundation/relayer-engine"
+import GenericRelayerPluginDef, { GenericRelayerPluginConfig } from "./plugin/src/plugin"
 
 async function main() {
+  console.log("args: ", process.argv)
+
   // load plugin config
+  const envType = selectPluginConfig(process.argv[2] || "")
   const pluginConfig = (await relayerEngine.loadFileAndParseToObject(
-    `./src/plugin/config/${relayerEngine.EnvType.DEVNET.toLowerCase()}.json`
-  )) as GenericRelayerPluginConfig;
+    `./src/plugin/config/${envType.toLowerCase()}.json`
+  )) as GenericRelayerPluginConfig
 
   // run relayer engine
   await relayerEngine.run({
-    configs: "./engine_config",
+    configs: "./engine_config/" + envType.toLowerCase(),
     plugins: [GenericRelayerPluginDef.init(pluginConfig)],
     mode: relayerEngine.Mode.BOTH,
-  });
+  })
+}
+
+function selectPluginConfig(flag: string) {
+  switch (flag) {
+    case "--testnet":
+      return relayerEngine.EnvType.DEVNET
+    case "--mainnet":
+      return relayerEngine.EnvType.MAINNET
+    case "--tilt":
+      return relayerEngine.EnvType.TILT
+    default:
+      return relayerEngine.EnvType.TILT
+  }
 }
 
 // allow main to be an async function and block until it rejects or resolves
 main().catch((e) => {
-  console.error(e);
-  process.exit(1);
-});
+  console.error(e)
+  process.exit(1)
+})
