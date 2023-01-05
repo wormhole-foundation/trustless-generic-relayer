ac22124e6cfe5126c39c5dc72e5a368531082979
Joe Howarth <josephehowarth@gmail.com>
2023-01-05 16:04:30 -0700

    add pkeys.sh to gitignore, fix imports (#24)

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,5 +1,3 @@
-export type { GasOracle } from "./ethers-contracts/GasOracle"
-export { GasOracle__factory } from "./ethers-contracts/factories/GasOracle__factory"
 export type { CoreRelayer } from "./ethers-contracts/CoreRelayer"
 export { CoreRelayer__factory } from "./ethers-contracts/factories/CoreRelayer__factory"
 export type { MockRelayerIntegration } from "./ethers-contracts/MockRelayerIntegration"
