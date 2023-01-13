4f83c5f3bc0985a809a39dd093ecf891597506e1
Joe Howarth <josephehowarth@gmail.com>
2023-01-12 18:00:38 -0700

    remove nested sdk

--- relayer_engine/sdk/sdk/src/index.ts
+++
@@ -1,8 +0,0 @@
-export type { CoreRelayer } from "./ethers-contracts/CoreRelayer"
-export { CoreRelayer__factory } from "./ethers-contracts/factories/CoreRelayer__factory"
-export type { MockRelayerIntegration } from "./ethers-contracts/MockRelayerIntegration"
-export { MockRelayerIntegration__factory } from "./ethers-contracts/factories/MockRelayerIntegration__factory"
-export type { IWormhole } from "./ethers-contracts/IWormhole"
-export { IWormhole__factory } from "./ethers-contracts/factories/IWormhole__factory"
-export type { RelayProvider } from "./ethers-contracts/RelayProvider"
-export { RelayProvider__factory } from "./ethers-contracts/factories/RelayProvider__factory"
