d89cbfc949a932b142a817be82dd62779b9f2347
Joe Howarth <josephehowarth@gmail.com>
2023-01-17 11:01:52 -0700

    First k8s deployment

--- relayer_engine/deploy/inject-private-keys.sh
+++ relayer_engine/deploy/inject-private-keys.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/sh
 
+# typically source ../../pkeys.sh in project root first
+
+# kubectl delete secret private-keys --ignore-not-found
+
 kubectl create secret generic private-keys \
     --from-literal=PRIVATE_KEYS_CHAIN_14=${PRIVATE_KEYS_CHAIN_14} \
     --from-literal=PRIVATE_KEYS_CHAIN_6=${PRIVATE_KEYS_CHAIN_6}
