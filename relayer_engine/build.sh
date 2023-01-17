c119f88024cf5fa658f57beb38bb70ca58cabaa7
Joe Howarth <josephehowarth@gmail.com>
2023-01-17 15:41:15 -0700

    save

--- relayer_engine/build.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/sh
-
-cp -r ../sdk ./sdk
-cp ../ethereum/ts-scripts/config/testnet/contracts.json .
---
+++ relayer_engine/deploy/simple-gr.sh
@@ -0,0 +1,2 @@
+cp ../../ethereum/ts-scripts/config/testnet/contracts.json ./k8s-testnet/contracts.json
+kubectl apply -k .
