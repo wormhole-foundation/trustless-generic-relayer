4cf88d3be6db7f10dafe8db8bc6791ad3c7b82c8
Joe Howarth <josephehowarth@gmail.com>
2023-01-27 14:44:45 -0700

    improve message test again

--- relayer_engine/deploy/inject-private-keys.sh
+++ relayer_engine/deploy/inject-private-keys.sh
@@ -8,4 +8,5 @@ kubectl create secret generic private-keys \
     --from-literal=PRIVATE_KEYS_CHAIN_4=${PRIVATE_KEYS_CHAIN_4} \
     --from-literal=PRIVATE_KEYS_CHAIN_5=${PRIVATE_KEYS_CHAIN_5} \
     --from-literal=PRIVATE_KEYS_CHAIN_6=${PRIVATE_KEYS_CHAIN_6} \
+    --from-literal=PRIVATE_KEYS_CHAIN_16=${PRIVATE_KEYS_CHAIN_16} \
     --from-literal=PRIVATE_KEYS_CHAIN_14=${PRIVATE_KEYS_CHAIN_14} 
