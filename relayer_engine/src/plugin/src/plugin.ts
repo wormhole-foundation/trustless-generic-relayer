76b06851b131f14b60e6e0a65c1dfd7d528ace9a
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-02-15 15:33:35 +0000

    Add fix to relayer engine not relaying not sufficiently funded messages. But Incomplete test; how to eventually relay these!

--- relayer_engine/src/plugin/src/plugin.ts
+++ relayer_engine/src/plugin/src/plugin.ts
@@ -517,7 +517,10 @@ export class GenericRelayerPlugin implements Plugin<WorkflowPayload> {
     const payload = this.parseWorkflowPayload(workflow)
     switch (payload.payloadId) {
       case RelayerPayloadId.Delivery:
-        return await this.handleDeliveryWorkflow(payload, execute)
+        if(payload.deliveryInstructionsContainer.sufficientlyFunded) {
+          return await this.handleDeliveryWorkflow(payload, execute)
+        }
+        return 
       case RelayerPayloadId.Redelivery:
         return await this.handleRedeliveryWorkflow(payload, execute)
     }
