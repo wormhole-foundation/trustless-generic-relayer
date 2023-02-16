5f06a4ef95c982183e57962533cf2636813a8000
Sebastián Claudio Nale <sebinale@gmail.com>
2023-02-16 19:50:56 -0300

    Adds constants for all literals in `BytesLib`.

--- ethereum/contracts/libraries/external/BytesLib.sol
+++ ethereum/contracts/libraries/external/BytesLib.sol
@@ -10,11 +10,28 @@ pragma solidity >=0.8.0 <0.9.0;
 
 library BytesLib {
     uint256 private constant freeMemoryPtr = 0x40;
-    function slice(bytes memory _bytes, uint256 _start, uint256 _length) internal pure returns (bytes memory) {
+    uint256 private constant mask32ByteAlignment = 0x1f;
+    /**
+     * Size of word read by `mload` instruction.
+     */
+    uint256 private constant memoryWord = 32;
+    uint256 private constant uint8Size = 1;
+    uint256 private constant uint16Size = 2;
+    uint256 private constant uint32Size = 4;
+    uint256 private constant uint64Size = 8;
+    uint256 private constant uint128Size = 16;
+    uint256 private constant uint256Size = 32;
+    uint256 private constant addressSize = 20;
+    /**
+     * Bits in 12 bytes.
+     */
+    uint256 private constant bytes12Bits = 96;
+
+    function slice(bytes memory buffer, uint256 startIndex, uint256 length) internal pure returns (bytes memory) {
         unchecked {
-            require(_length + 31 >= _length, "slice_overflow");
+            require(length + 31 >= length, "slice_overflow");
         }
-        require(_bytes.length >= _start + _length, "slice_outOfBounds");
+        require(buffer.length >= startIndex + length, "slice_outOfBounds");
 
         bytes memory tempBytes;
 
@@ -23,7 +40,7 @@ library BytesLib {
             // Solidity does for memory variables.
             tempBytes := mload(freeMemoryPtr)
 
-            switch iszero(_length)
+            switch iszero(length)
             case 0 {
                 // The first word of the slice result is potentially a partial
                 // word read from the original array. To read it, we calculate
@@ -33,126 +50,132 @@ library BytesLib {
                 // land at the beginning of the contents of the new array. When
                 // we're done copying, we overwrite the full first word with
                 // the actual length of the slice.
-                let lengthmod := and(_length, 31)
+                let lengthmod := and(length, mask32ByteAlignment)
                 // The multiplication in the next line is necessary
                 // because when slicing multiples of 32 bytes (lengthmod == 0)
                 // the following copy loop was copying the origin's length
                 // and then ending prematurely not copying everything it should.
-                let startOffset := add(lengthmod, mul(32, iszero(lengthmod)))
+                let startOffset := add(lengthmod, mul(memoryWord, iszero(lengthmod)))
 
                 let dst := add(tempBytes, startOffset)
-                let end := add(dst, _length)
+                let end := add(dst, length)
 
-                for { let src := add(add(_bytes, startOffset), _start) } lt(dst, end) {
-                    dst := add(dst, 32)
-                    src := add(src, 32)
+                for { let src := add(add(buffer, startOffset), startIndex) } lt(dst, end) {
+                    dst := add(dst, memoryWord)
+                    src := add(src, memoryWord)
                 } { mstore(dst, mload(src)) }
 
 
                 //update free-memory pointer
                 //allocating the array padded to 32 bytes like the compiler does now
-                mstore(freeMemoryPtr, and(add(dst, 31), not(31)))
+                mstore(freeMemoryPtr, and(add(dst, mask32ByteAlignment), not(mask32ByteAlignment)))
             }
             //if we want a zero-length slice let's just return a zero-length array
             default {
-                mstore(freeMemoryPtr, add(tempBytes, 32))
+                mstore(freeMemoryPtr, add(tempBytes, memoryWord))
             }
 
             // Store the length of the buffer
             // We need to do it even if the length is zero because Solidity does not garbage collect
-            mstore(tempBytes, _length)
+            mstore(tempBytes, length)
         }
 
         return tempBytes;
     }
 
-    function toAddress(bytes memory _bytes, uint256 _start) internal pure returns (address) {
-        require(_bytes.length >= _start + 20, "toAddress_outOfBounds");
+    function toAddress(bytes memory buffer, uint256 startIndex) internal pure returns (address) {
+        require(buffer.length >= startIndex + addressSize, "toAddress_outOfBounds");
         address tempAddress;
 
         assembly ("memory-safe") {
             // We want to shift into the lower 12 bytes and leave the upper 12 bytes clear.
-            // 12 bytes * 8 == 96 bits
-            tempAddress := shr(96, mload(add(add(_bytes, 0x20), _start)))
+            tempAddress := shr(bytes12Bits, mload(add(add(buffer, memoryWord), startIndex)))
         }
 
         return tempAddress;
     }
 
-    function toUint8(bytes memory _bytes, uint256 _start) internal pure returns (uint8) {
-        require(_bytes.length > _start, "toUint8_outOfBounds");
+    function toUint8(bytes memory buffer, uint256 startIndex) internal pure returns (uint8) {
+        require(buffer.length > startIndex, "toUint8_outOfBounds");
+
+        // Note that `endIndex == startOffset` for a given buffer due to the 32 bytes at the start that store the length.
+        uint256 startOffset = startIndex + uint8Size;
         uint8 tempUint;
-
         assembly ("memory-safe") {
-            tempUint := mload(add(add(_bytes, 0x1), _start))
+            tempUint := mload(add(buffer, startOffset))
         }
-
         return tempUint;
     }
 
-    function toUint16(bytes memory _bytes, uint256 _start) internal pure returns (uint16) {
-        require(_bytes.length >= _start + 2, "toUint16_outOfBounds");
+    function toUint16(bytes memory buffer, uint256 startIndex) internal pure returns (uint16) {
+        uint256 endIndex = startIndex + uint16Size;
+        require(buffer.length >= endIndex, "toUint16_outOfBounds");
+
         uint16 tempUint;
-
         assembly ("memory-safe") {
-            tempUint := mload(add(add(_bytes, 0x2), _start))
+            // Note that `endIndex == startOffset` for a given buffer due to the 32 bytes at the start that store the length.
+            tempUint := mload(add(buffer, endIndex))
         }
-
         return tempUint;
     }
 
-    function toUint32(bytes memory _bytes, uint256 _start) internal pure returns (uint32) {
-        require(_bytes.length >= _start + 4, "toUint32_outOfBounds");
+    function toUint32(bytes memory buffer, uint256 startIndex) internal pure returns (uint32) {
+        uint256 endIndex = startIndex + uint32Size;
+        require(buffer.length >= endIndex, "toUint32_outOfBounds");
+
         uint32 tempUint;
-
         assembly ("memory-safe") {
-            tempUint := mload(add(add(_bytes, 0x4), _start))
+            // Note that `endIndex == startOffset` for a given buffer due to the 32 bytes at the start that store the length.
+            tempUint := mload(add(buffer, endIndex))
         }
-
         return tempUint;
     }
 
-    function toUint64(bytes memory _bytes, uint256 _start) internal pure returns (uint64) {
-        require(_bytes.length >= _start + 8, "toUint64_outOfBounds");
+    function toUint64(bytes memory buffer, uint256 startIndex) internal pure returns (uint64) {
+        uint256 endIndex = startIndex + uint64Size;
+        require(buffer.length >= endIndex, "toUint64_outOfBounds");
+
         uint64 tempUint;
-
         assembly ("memory-safe") {
-            tempUint := mload(add(add(_bytes, 0x8), _start))
+            // Note that `endIndex == startOffset` for a given buffer due to the 32 bytes at the start that store the length.
+            tempUint := mload(add(buffer, endIndex))
         }
-
         return tempUint;
     }
 
-    function toUint128(bytes memory _bytes, uint256 _start) internal pure returns (uint128) {
-        require(_bytes.length >= _start + 16, "toUint128_outOfBounds");
+    function toUint128(bytes memory buffer, uint256 startIndex) internal pure returns (uint128) {
+        uint256 endIndex = startIndex + uint128Size;
+        require(buffer.length >= endIndex, "toUint128_outOfBounds");
+
         uint128 tempUint;
-
         assembly ("memory-safe") {
-            tempUint := mload(add(add(_bytes, 0x10), _start))
+            // Note that `endIndex == startOffset` for a given buffer due to the 32 bytes at the start that store the length.
+            tempUint := mload(add(buffer, endIndex))
         }
-
         return tempUint;
     }
 
-    function toUint256(bytes memory _bytes, uint256 _start) internal pure returns (uint256) {
-        require(_bytes.length >= _start + 32, "toUint256_outOfBounds");
+    function toUint256(bytes memory buffer, uint256 startIndex) internal pure returns (uint256) {
+        uint256 endIndex = startIndex + uint256Size;
+        require(buffer.length >= endIndex, "toUint256_outOfBounds");
+
         uint256 tempUint;
-
         assembly ("memory-safe") {
-            tempUint := mload(add(add(_bytes, 0x20), _start))
+            // Note that `endIndex == startOffset` for a given buffer due to the 32 bytes at the start that store the length.
+            tempUint := mload(add(buffer, endIndex))
         }
-
         return tempUint;
     }
 
-    function toBytes32(bytes memory _bytes, uint256 _start) internal pure returns (bytes32) {
-        require(_bytes.length >= _start + 32, "toBytes32_outOfBounds");
+    function toBytes32(bytes memory buffer, uint256 startIndex) internal pure returns (bytes32) {
+        uint256 endIndex = startIndex + uint256Size;
+        require(buffer.length >= endIndex, "toBytes32_outOfBounds");
+
         bytes32 tempBytes32;
-
         assembly ("memory-safe") {
-            tempBytes32 := mload(add(add(_bytes, 0x20), _start))
+            // Note that `endIndex == startOffset` for a given buffer due to the 32 bytes at the start that store the length.
+            tempBytes32 := mload(add(buffer, endIndex))
         }
-
         return tempBytes32;
     }
 }
