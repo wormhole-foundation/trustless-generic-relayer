ec3022ba90f3af5dd259cf9baa767607e2710bb8
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-02-23 21:39:39 +0000

    Move encode/decode/convert-request-to-instruction related files to CoreRelayerMessages

--- ethereum/contracts/coreRelayer/CoreRelayerMessages.sol
+++ ethereum/contracts/coreRelayer/CoreRelayerMessages.sol
@@ -16,6 +16,189 @@ contract CoreRelayerMessages is CoreRelayerStructs, CoreRelayerGetters {
     error InvalidDeliveryInstructionsPayload(uint256 length);
     error InvalidSendsPayload(uint256 length);
 
+    function convertToEncodedRedeliveryByTxHashInstruction(
+        IWormholeRelayer.ResendByTx memory request,
+        uint256 receiverValueTarget,
+        uint256 maximumRefund,
+        uint16 targetChain, 
+        uint256 newMaxTransactionFee,
+        IRelayProvider provider
+    ) internal view returns (bytes memory encoded) {
+        encoded = abi.encodePacked(
+            uint8(2), //version payload number
+            uint16(request.sourceChain),
+            bytes32(request.sourceTxHash),
+            uint32(request.sourceNonce),
+            uint16(request.targetChain),
+            uint8(request.deliveryIndex),
+            uint8(request.multisendIndex),
+            maximumRefund,
+            receiverValueTarget,
+            uint8(1), //version for ExecutionParameters
+            calculateTargetGasRedeliveryAmount(targetChain, newMaxTransactionFee, provider),
+            provider.getDeliveryAddress(request.targetChain)
+        );
+    }
+
+    function convertToEncodedDeliveryInstructions(IWormholeRelayer.MultichainSend memory container, bool isFunded)
+        internal
+        view
+        returns (bytes memory encoded)
+    {
+        encoded = abi.encodePacked(
+            uint8(1), //version payload number
+            uint8(isFunded ? 1 : 0), // sufficiently funded
+            uint8(container.requests.length) //number of requests in the array
+        );
+
+        // TODO: this probably results in a quadratic algorithm. Further optimization can be done here.
+        // Append all the messages to the array.
+        for (uint256 i = 0; i < container.requests.length; i++) {
+            encoded = appendDeliveryInstruction(
+                encoded, container.requests[i], IRelayProvider(container.relayProviderAddress)
+            );
+        }
+    }
+
+    function appendDeliveryInstruction(bytes memory encoded, IWormholeRelayer.Send memory request, IRelayProvider provider)
+        internal
+        view
+        returns (bytes memory newEncoded)
+    {
+        newEncoded = abi.encodePacked(
+            encoded,
+            request.targetChain,
+            request.targetAddress,
+            request.refundAddress,
+            calculateTargetDeliveryMaximumRefund(request.targetChain, request.maxTransactionFee, provider),
+            convertApplicationBudgetAmount(request.receiverValue, request.targetChain, provider),
+            uint8(1), //version for ExecutionParameters
+            calculateTargetGasDeliveryAmount(request.targetChain, request.maxTransactionFee, provider),
+            provider.getDeliveryAddress(request.targetChain)
+        );
+    }
+
+     /**
+     * Given a targetChain, maxTransactionFee, and a relay provider, this function calculates what the gas limit of the delivery transaction
+     * should be.
+     */
+    function calculateTargetGasDeliveryAmount(uint16 targetChain, uint256 maxTransactionFee, IRelayProvider provider)
+        internal
+        view
+        returns (uint32 gasAmount)
+    {
+        gasAmount = calculateTargetGasDeliveryAmountHelper(
+            targetChain, maxTransactionFee, provider.quoteDeliveryOverhead(targetChain), provider
+        );
+    }
+
+    function calculateTargetDeliveryMaximumRefund(
+        uint16 targetChain,
+        uint256 maxTransactionFee,
+        IRelayProvider provider
+    ) internal view returns (uint256 maximumRefund) {
+        maximumRefund = calculateTargetDeliveryMaximumRefundHelper(
+            targetChain, maxTransactionFee, provider.quoteDeliveryOverhead(targetChain), provider
+        );
+    }
+
+    /**
+     * Given a targetChain, maxTransactionFee, and a relay provider, this function calculates what the gas limit of the redelivery transaction
+     * should be.
+     */
+    function calculateTargetGasRedeliveryAmount(uint16 targetChain, uint256 maxTransactionFee, IRelayProvider provider)
+        internal
+        view
+        returns (uint32 gasAmount)
+    {
+        gasAmount = calculateTargetGasDeliveryAmountHelper(
+            targetChain, maxTransactionFee, provider.quoteRedeliveryOverhead(targetChain), provider
+        );
+    }
+
+    function calculateTargetRedeliveryMaximumRefund(
+        uint16 targetChain,
+        uint256 maxTransactionFee,
+        IRelayProvider provider
+    ) internal view returns (uint256 maximumRefund) {
+        maximumRefund = calculateTargetDeliveryMaximumRefundHelper(
+            targetChain, maxTransactionFee, provider.quoteRedeliveryOverhead(targetChain), provider
+        );
+    }
+
+    function calculateTargetGasDeliveryAmountHelper(
+        uint16 targetChain,
+        uint256 maxTransactionFee,
+        uint256 deliveryOverhead,
+        IRelayProvider provider
+    ) internal view returns (uint32 gasAmount) {
+        if (maxTransactionFee <= deliveryOverhead) {
+            gasAmount = 0;
+        } else {
+            uint256 gas = (maxTransactionFee - deliveryOverhead) / provider.quoteGasPrice(targetChain);
+            if (gas > type(uint32).max) {
+                gasAmount = type(uint32).max;
+            } else {
+                gasAmount = uint32(gas);
+            }
+        }
+    }
+
+    function calculateTargetDeliveryMaximumRefundHelper(
+        uint16 targetChain,
+        uint256 maxTransactionFee,
+        uint256 deliveryOverhead,
+        IRelayProvider provider
+    ) internal view returns (uint256 maximumRefund) {
+        if (maxTransactionFee >= deliveryOverhead) {
+            uint256 remainder = maxTransactionFee - deliveryOverhead;
+            maximumRefund = assetConversionHelper(chainId(), remainder, targetChain, 1, 1, false, provider);
+        } else {
+            maximumRefund = 0;
+        }
+    }
+
+    function assetConversionHelper(
+        uint16 sourceChain,
+        uint256 sourceAmount,
+        uint16 targetChain,
+        uint256 multiplier,
+        uint256 multiplierDenominator,
+        bool roundUp,
+        IRelayProvider provider
+    ) internal view returns (uint256 targetAmount) {
+        uint256 srcNativeCurrencyPrice = provider.quoteAssetPrice(sourceChain);
+        if (srcNativeCurrencyPrice == 0) {
+            revert IWormholeRelayer.RelayProviderDoesNotSupportTargetChain();
+        }
+
+        uint256 dstNativeCurrencyPrice = provider.quoteAssetPrice(targetChain);
+        if (dstNativeCurrencyPrice == 0) {
+            revert IWormholeRelayer.RelayProviderDoesNotSupportTargetChain();
+        }
+        uint256 numerator = sourceAmount * srcNativeCurrencyPrice * multiplier;
+        uint256 denominator = dstNativeCurrencyPrice * multiplierDenominator;
+        if (roundUp) {
+            targetAmount = (numerator + denominator - 1) / denominator;
+        } else {
+            targetAmount = numerator / denominator;
+        }
+    }
+
+     //This should invert quoteApplicationBudgetAmount, I.E when a user pays the sourceAmount, they receive at least the value of targetAmount they requested from
+    //quoteReceiverValue.
+    function convertApplicationBudgetAmount(uint256 sourceAmount, uint16 targetChain, IRelayProvider provider)
+        internal
+        view
+        returns (uint256 targetAmount)
+    {
+        (uint16 buffer, uint16 denominator) = provider.getAssetConversionBuffer(targetChain);
+
+        targetAmount = assetConversionHelper(
+            chainId(), sourceAmount, targetChain, denominator, uint256(0) + denominator + buffer, false, provider
+        );
+    }
+
     function decodeRedeliveryByTxHashInstruction(bytes memory encoded)
         internal
         pure
@@ -24,6 +207,9 @@ contract CoreRelayerMessages is CoreRelayerStructs, CoreRelayerGetters {
         uint256 index = 0;
 
         instruction.payloadId = encoded.toUint8(index);
+        if (instruction.payloadId != 2) {
+            revert InvalidPayloadId(instruction.payloadId);
+        }
         index += 1;
 
         instruction.sourceChain = encoded.toUint16(index);
