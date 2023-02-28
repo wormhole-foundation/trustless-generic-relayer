6f7199eb7ac85ec46c9f20d0098ef46e98724372
Gabriel Zimmermann <gabrielemanuel@gmail.com>
2023-02-28 13:52:03 -0400

    Adds k8s namespace

--- relayer_engine/deploy/inject-private-keys.sh
+++ relayer_engine/deploy/inject-private-keys.sh
@@ -2,11 +2,12 @@
 
 # typically source ../../pkeys.sh in project root first
 
-kubectl delete secret private-keys --ignore-not-found
+kubectl delete secret private-keys --ignore-not-found --namespace generic-relayer
 
 kubectl create secret generic private-keys \
     --from-literal=PRIVATE_KEYS_CHAIN_4=${PRIVATE_KEYS_CHAIN_4} \
     --from-literal=PRIVATE_KEYS_CHAIN_5=${PRIVATE_KEYS_CHAIN_5} \
     --from-literal=PRIVATE_KEYS_CHAIN_6=${PRIVATE_KEYS_CHAIN_6} \
     --from-literal=PRIVATE_KEYS_CHAIN_16=${PRIVATE_KEYS_CHAIN_16} \
-    --from-literal=PRIVATE_KEYS_CHAIN_14=${PRIVATE_KEYS_CHAIN_14} 
+    --from-literal=PRIVATE_KEYS_CHAIN_14=${PRIVATE_KEYS_CHAIN_14} \
+    --namespace generic-relayer
