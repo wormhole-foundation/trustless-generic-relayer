345f69ace26e550dff1f90e886c3139de8613191
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-03-09 17:45:30 +0000

    update SDK in relayer engine

--- relayer_engine/pkgs/sdk/src/main/status.ts
+++ relayer_engine/pkgs/sdk/src/main/status.ts
@@ -1,33 +1,49 @@
 import {
   ChainId,
   CHAIN_ID_TO_NAME,
+  isChain,
   CONTRACTS,
   getSignedVAAWithRetry,
   Network,
   parseVaa,
+  ParsedVaa,
   tryNativeToHexString,
 } from "@certusone/wormhole-sdk"
 import { GetSignedVAAResponse } from "@certusone/wormhole-sdk-proto-web/lib/cjs/publicrpc/v1/publicrpc"
 import { Implementation__factory } from "@certusone/wormhole-sdk/lib/cjs/ethers-contracts"
 import { BigNumber, ContractReceipt, ethers, providers } from "ethers"
-import { getCoreRelayer, getCoreRelayerAddressNative, RPCS_BY_CHAIN, GUARDIAN_RPC_HOSTS } from "../consts"
-//import { CoreRelayer__factory } from "../ethers-contracts"
 import {
-  DeliveryEvent,
-} from "../ethers-contracts/CoreRelayer"
+  getCoreRelayer,
+  getCoreRelayerAddressNative,
+  RPCS_BY_CHAIN,
+  GUARDIAN_RPC_HOSTS,
+} from "../consts"
+import {
+  parsePayloadType,
+  RelayerPayloadId,
+  parseDeliveryInstructionsContainer,
+  parseRedeliveryByTxHashInstruction,
+  DeliveryInstruction,
+  DeliveryInstructionsContainer,
+  RedeliveryByTxHashInstruction,
+  ExecutionParameters,
+} from "../structs"
+import { DeliveryEvent } from "../ethers-contracts/CoreRelayer"
 
-type DeliveryStatus =
-  | "Waiting for VAA"
-  | "Pending Delivery"
-  | "Delivery Success"
-  | "Receiver Failure"
-  | "Invalid Redelivery"
-  | "Forward Request Success"
-  | "Forward Request Failure"
-  | "This should never happen. Contact Support."
+enum DeliveryStatus {
+  WaitingForVAA = "Waiting for VAA",
+  PendingDelivery = "Pending Delivery",
+  DeliverySuccess = "Delivery Success",
+  ReceiverFailure = "Receiver Failure",
+  InvalidRedelivery = "Invalid Redelivery",
+  ForwardRequestSuccess = "Forward Request Success",
+  ForwardRequestFailure = "Forward Request Failure",
+  ThisShouldNeverHappen = "This should never happen. Contact Support.",
+  DeliveryDidntHappenWithinRange = "Delivery didn't happen within given block range",
+}
 
-type DeliveryInfo = {
-  status: DeliveryStatus,
+type DeliveryTargetInfo = {
+  status: DeliveryStatus
   deliveryTxHash: string | null
   vaaHash: string | null
   sourceChain: number | null
@@ -35,23 +51,115 @@ type DeliveryInfo = {
 }
 
 type InfoRequest = {
-  environment: Network,
-  sourceChain: ChainId,
-  sourceTransaction: string,
-  sourceChainProvider?: ethers.providers.Provider,
-  targetChain: ChainId,
-  targetChainProvider?: ethers.providers.Provider,
-  sourceNonce?: number,
+  environment: Network
+  sourceChain: ChainId
+  sourceTransaction: string
+  sourceChainProvider?: ethers.providers.Provider
+  targetChainProviders?: Map<number, ethers.providers.Provider>
+  sourceNonce?: number
   coreRelayerWhMessageIndex?: number
 }
 
-export async function getDeliveryInfoBySourceTx(infoRequest: InfoRequest): Promise<DeliveryInfo[]> {
-  const sourceChainProvider = infoRequest.sourceChainProvider || new ethers.providers.StaticJsonRpcProvider(RPCS_BY_CHAIN[CHAIN_ID_TO_NAME[infoRequest.sourceChain]]);
-  if(!sourceChainProvider) throw Error("No default RPC for this chain; pass in your own provider (as sourceChainProvider)")
-  const receipt = await sourceChainProvider.getTransactionReceipt(infoRequest.sourceTransaction)
-  if(!receipt) throw Error("Transaction has not been mined")
-  const bridgeAddress = CONTRACTS[infoRequest.environment][CHAIN_ID_TO_NAME[infoRequest.sourceChain]].core
-  const coreRelayerAddress = getCoreRelayerAddressNative(infoRequest.sourceChain, infoRequest.environment)
+export function parseWormholeLog(log: ethers.providers.Log): {
+  type: RelayerPayloadId
+  parsed: DeliveryInstructionsContainer | RedeliveryByTxHashInstruction | string
+} {
+  const abi = [
+    "event LogMessagePublished(address indexed sender, uint64 sequence, uint32 nonce, bytes payload, uint8 consistencyLevel);",
+  ]
+  const iface = new ethers.utils.Interface(abi)
+  const parsed = iface.parseLog(log)
+  const payload = Buffer.from(parsed.args.payload.substring(2), "hex")
+  const type = parsePayloadType(payload)
+  if (type == RelayerPayloadId.Delivery) {
+    return { type, parsed: parseDeliveryInstructionsContainer(payload) }
+  } else if (type == RelayerPayloadId.Redelivery) {
+    return { type, parsed: parseRedeliveryByTxHashInstruction(payload) }
+  } else {
+    return { type: -1, parsed: "Invalid wormhole message" }
+  }
+}
+
+export type DeliveryInfo = {
+  type: RelayerPayloadId.Delivery
+  sourceChainId: ChainId,
+  sourceTransactionHash: string
+  deliveryInstructionsContainer: DeliveryInstructionsContainer
+  targetChainStatuses: {
+    chainId: ChainId
+    events: { status: DeliveryStatus; transactionHash: string | null }[]
+  }[]
+}
+
+export type RedeliveryInfo = {
+  type: RelayerPayloadId.Redelivery
+  redeliverySourceChainId: ChainId,
+  redeliverySourceTransactionHash: string
+  redeliveryInstruction: RedeliveryByTxHashInstruction
+}
+
+export function printChain(chainId: number) {
+  return `${CHAIN_ID_TO_NAME[chainId as ChainId]} (Chain ${chainId})`
+}
+
+export function printInfo(info: DeliveryInfo | RedeliveryInfo) {
+  if(info.type==RelayerPayloadId.Redelivery) {
+    console.log(`Found Redelivery request in transaction ${info.redeliverySourceTransactionHash} on ${printChain(info.redeliverySourceChainId)}`)
+    console.log(`Original Delivery Source Chain: ${printChain(info.redeliveryInstruction.sourceChain)}`)
+    console.log(`Original Delivery Source Transaction Hash: 0x${info.redeliveryInstruction.sourceTxHash.toString("hex")}`)
+    console.log(`Original Delivery Source Nonce: ${info.redeliveryInstruction.sourceNonce}`)
+    console.log(`Target Chain: ${printChain(info.redeliveryInstruction.targetChain)}`)
+    console.log(`multisendIndex: ${info.redeliveryInstruction.multisendIndex}`)
+    console.log(`deliveryIndex: ${info.redeliveryInstruction.deliveryIndex}`)
+    console.log(`New max amount (in target chain currency) to use for gas: ${info.redeliveryInstruction.newMaximumRefundTarget}`)
+    console.log(`New amount (in target chain currency) to pass into target address: ${info.redeliveryInstruction.newMaximumRefundTarget}`)
+    console.log(`New target chain gas limit: ${info.redeliveryInstruction.executionParameters.gasLimit}`)
+    console.log(`Relay Provider Delivery Address: 0x${info.redeliveryInstruction.executionParameters.providerDeliveryAddress.toString("hex")}`)
+  } else if(info.type==RelayerPayloadId.Delivery) {
+    console.log(`Found delivery request in transaction ${info.sourceTransactionHash} on ${printChain(info.sourceChainId)}`)
+    console.log((info.deliveryInstructionsContainer.sufficientlyFunded ? "The delivery was funded" : "** NOTE: The delivery was NOT sufficiently funded. You did not have enough leftover funds to perform the forward **"))
+    const length = info.deliveryInstructionsContainer.instructions.length;
+    console.log(`\nMessages were requested to be sent to ${length} destination${length == 1 ? "" : "s"}:`)
+    console.log(info.deliveryInstructionsContainer.instructions.map((instruction: DeliveryInstruction, i) => {
+      let result = "";
+      const targetChainName = CHAIN_ID_TO_NAME[instruction.targetChain as ChainId];
+      result += `\n(Destination ${i}): Target address is 0x${instruction.targetAddress.toString("hex")} on ${printChain(instruction.targetChain)}\n`
+      result += `Max amount to use for gas: ${instruction.maximumRefundTarget} of ${targetChainName} currency\n`
+      result += instruction.receiverValueTarget.gt(0) ? `Amount to pass into target address: ${instruction.receiverValueTarget} of ${CHAIN_ID_TO_NAME[instruction.targetChain as ChainId]} currency\n` : ``
+      result += `Gas limit: ${instruction.executionParameters.gasLimit} ${targetChainName} gas\n`
+      result += `Relay Provider Delivery Address: 0x${instruction.executionParameters.providerDeliveryAddress.toString("hex")}\n`
+      result += info.targetChainStatuses[i].events.map((e, i) => (`Delivery attempt ${i+1}: ${e.status}${e.transactionHash ? ` (${targetChainName} transaction hash: ${e.transactionHash})` : ""}`)).join("\n")
+      return result;
+    }).join("\n"))
+  }
+}
+
+export async function getDeliveryInfoBySourceTx(
+  infoRequest: InfoRequest
+): Promise<DeliveryInfo | RedeliveryInfo> {
+  const sourceChainProvider =
+    infoRequest.sourceChainProvider ||
+    new ethers.providers.StaticJsonRpcProvider(
+      RPCS_BY_CHAIN[infoRequest.environment][CHAIN_ID_TO_NAME[infoRequest.sourceChain]]
+    )
+  if (!sourceChainProvider)
+    throw Error(
+      "No default RPC for this chain; pass in your own provider (as sourceChainProvider)"
+    )
+  console.log(
+    "Default RPC: " +
+      RPCS_BY_CHAIN[infoRequest.environment][CHAIN_ID_TO_NAME[infoRequest.sourceChain]]
+  )
+  const receipt = await sourceChainProvider.getTransactionReceipt(
+    infoRequest.sourceTransaction
+  )
+  if (!receipt) throw Error("Transaction has not been mined")
+  const bridgeAddress =
+    CONTRACTS[infoRequest.environment][CHAIN_ID_TO_NAME[infoRequest.sourceChain]].core
+  const coreRelayerAddress = getCoreRelayerAddressNative(
+    infoRequest.sourceChain,
+    infoRequest.environment
+  )
   if (!bridgeAddress || !coreRelayerAddress) {
     throw Error("Invalid chain ID or network")
   }
@@ -64,80 +172,131 @@ export async function getDeliveryInfoBySourceTx(infoRequest: InfoRequest): Promi
     infoRequest.sourceNonce?.toString()
   )
 
-  //const parsedInstruction = CoreRelayer__factory.connect(coreRelayerAddress, sourceChainProvider).decodeDeliveryInstructionsContainer(deliveryLog.log.data);
-  //console.log(parsedInstruction)
+  const { type, parsed } = parseWormholeLog(deliveryLog.log)
+
+  if (type == RelayerPayloadId.Redelivery) {
+    const redeliveryInstruction = parsed as RedeliveryByTxHashInstruction
+    return {
+      type,
+      redeliverySourceChainId: infoRequest.sourceChain,
+      redeliverySourceTransactionHash: infoRequest.sourceTransaction,
+      redeliveryInstruction,
+    }
+  }
 
   /* Potentially use 'guardianRPCHosts' to get status of VAA; code in comments at end [1] */
 
-  const targetChainProvider = infoRequest.targetChainProvider || new ethers.providers.StaticJsonRpcProvider(RPCS_BY_CHAIN[CHAIN_ID_TO_NAME[infoRequest.targetChain]]);
-  if(!targetChainProvider) throw Error("No default RPC for this chain; pass in your own provider (as targetChainProvider)")
+  const deliveryInstructionsContainer = parsed as DeliveryInstructionsContainer
 
-  const deliveryEvents = await pullEventsBySourceSequence(
-    infoRequest.environment,
-    infoRequest.targetChain,
-    targetChainProvider,
-    infoRequest.sourceChain,
-    BigNumber.from(deliveryLog.sequence)
-  )
-  if (deliveryEvents.length == 0) {
-    deliveryEvents.push({
-      status: "Pending Delivery",
-      deliveryTxHash: null,
-      vaaHash: null,
-      sourceChain: infoRequest.sourceChain,
-      sourceVaaSequence: BigNumber.from(deliveryLog.sequence),
-    })
+  const targetChainStatuses = await Promise.all(deliveryInstructionsContainer.instructions.map(async (instruction: DeliveryInstruction) => {
+    const targetChain = instruction.targetChain as ChainId;
+    if(!isChain(targetChain)) throw Error("Invalid Chain")
+    const targetChainProvider = 
+      infoRequest.targetChainProviders?.get(targetChain) ||
+      new ethers.providers.StaticJsonRpcProvider(
+        RPCS_BY_CHAIN[infoRequest.environment][CHAIN_ID_TO_NAME[targetChain]]
+      )
+
+    if (!targetChainProvider)
+      throw Error(
+        "No default RPC for this chain; pass in your own provider (as targetChainProvider)"
+      )
+      const sourceChainBlock = await sourceChainProvider.getBlock(receipt.blockNumber);
+    const deliveryEvents = await pullEventsBySourceSequence(
+      infoRequest.environment,
+      targetChain,
+      targetChainProvider,
+      infoRequest.sourceChain,
+      BigNumber.from(deliveryLog.sequence),
+      sourceChainBlock.timestamp
+    )
+    if (deliveryEvents.length == 0) {
+      deliveryEvents.push({
+        status: DeliveryStatus.DeliveryDidntHappenWithinRange,
+        deliveryTxHash: null,
+        vaaHash: null,
+        sourceChain: infoRequest.sourceChain,
+        sourceVaaSequence: BigNumber.from(deliveryLog.sequence),
+      })
+    }
+    return {
+      chainId: targetChain,
+      events: deliveryEvents.map((e)=>({status: e.status, transactionHash: e.deliveryTxHash}))
+    }
+  }))
+
+  return {
+    type,
+    sourceChainId: infoRequest.sourceChain,
+    sourceTransactionHash: infoRequest.sourceTransaction,
+    deliveryInstructionsContainer,
+    targetChainStatuses
   }
-
-  return deliveryEvents
 }
 
+function getBlockRange(provider: ethers.providers.Provider, timestamp: number): [number | string, number | string] {
+
+  return [0, "latest"]
+}
 
 async function pullEventsBySourceSequence(
   environment: Network,
   targetChain: ChainId,
   targetChainProvider: ethers.providers.Provider,
   sourceChain: number,
-  sourceVaaSequence: BigNumber
-): Promise<DeliveryInfo[]> {
+  sourceVaaSequence: BigNumber,
+  approximateTimestamp: number
+): Promise<DeliveryTargetInfo[]> {
   const coreRelayer = getCoreRelayer(targetChain, environment, targetChainProvider)
-  
+
+  const [blockRangeStart, blockRangeEnd] = getBlockRange(targetChainProvider, approximateTimestamp)
+
   //TODO These compile errors on sourceChain look like an ethers bug
-  const deliveryEvents = coreRelayer.filters.Delivery(null, sourceChain, sourceVaaSequence)
+  const deliveryEvents = coreRelayer.filters.Delivery(
+    null,
+    sourceChain,
+    sourceVaaSequence
+  )
 
   // There is a max limit on RPCs sometimes for how many blocks to query
   return await transformDeliveryEvents(
-    await coreRelayer.queryFilter(deliveryEvents, 0, 'latest'), targetChainProvider
+    await coreRelayer.queryFilter(deliveryEvents, blockRangeStart, blockRangeEnd),
+    targetChainProvider
   )
 }
 
 function deliveryStatus(status: number) {
   switch (status) {
-    case 0: 
-      return "Delivery Success"
+    case 0:
+      return DeliveryStatus.DeliverySuccess
     case 1:
-      return "Receiver Failure"
+      return DeliveryStatus.ReceiverFailure
     case 2:
-      return "Forward Request Failure"
+      return DeliveryStatus.ForwardRequestFailure
     case 3:
-      return "Forward Request Success"
-    case 4: 
-      return "Invalid Redelivery"
+      return DeliveryStatus.ForwardRequestSuccess
+    case 4:
+      return DeliveryStatus.InvalidRedelivery
     default:
-      return "This should never happen. Contact Support."
+      return DeliveryStatus.ThisShouldNeverHappen
   }
 }
 
-async function transformDeliveryEvents(events: DeliveryEvent[], targetProvider: ethers.providers.Provider): Promise<DeliveryInfo[]> {
-  return Promise.all(events.map(async (x) => {
-    return {
-      status: deliveryStatus(x.args[4]),
-      deliveryTxHash: x.transactionHash,
-      vaaHash: x.args[3],
-      sourceVaaSequence: x.args[2],
-      sourceChain: x.args[1],
-    }
-  }))
+async function transformDeliveryEvents(
+  events: DeliveryEvent[],
+  targetProvider: ethers.providers.Provider
+): Promise<DeliveryTargetInfo[]> {
+  return Promise.all(
+    events.map(async (x) => {
+      return {
+        status: deliveryStatus(x.args[4]),
+        deliveryTxHash: x.transactionHash,
+        vaaHash: x.args[3],
+        sourceVaaSequence: x.args[2],
+        sourceChain: x.args[1],
+      }
+    })
+  )
 }
 
 export function findLog(
@@ -145,7 +304,7 @@ export function findLog(
   bridgeAddress: string,
   emitterAddress: string,
   index: number,
-  nonce?: string,
+  nonce?: string
 ): { log: ethers.providers.Log; sequence: string } {
   const bridgeLogs = receipt.logs.filter((l) => {
     return l.address === bridgeAddress
@@ -162,12 +321,13 @@ export function findLog(
       nonce: log.args[2].toString(),
       emitterAddress: tryNativeToHexString(log.args[0].toString(), "ethereum"),
       log: bridgeLog,
-    };
+    }
   })
 
   const filtered = parsed.filter(
     (x) =>
-      x.emitterAddress == emitterAddress.toLowerCase() && ((!nonce) || (x.nonce == nonce.toLowerCase()))
+      x.emitterAddress == emitterAddress.toLowerCase() &&
+      (!nonce || x.nonce == nonce.toLowerCase())
   )
 
   if (filtered.length == 0) {
@@ -219,14 +379,13 @@ export function findLog(
   }
   */
 
-
 /*
 export async function getDeliveryInfoByVaaHash(
   environment: Network,
   targetChain: ChainId,
   targetChainProvider: ethers.providers.Provider,
   vaaHash: string
-): Promise<DeliveryInfo[]> {
+): Promise<DeliveryTargetInfo[]> {
   const deliveryEventInfos = await pullEventsByVaaHash(
     environment,
     targetChain,
@@ -252,7 +411,7 @@ async function pullEventsByVaaHash(
   targetChain: ChainId,
   targetChainProvider: ethers.providers.Provider,
   vaaHash: string
-): Promise<DeliveryInfo[]> {
+): Promise<DeliveryTargetInfo[]> {
   const coreRelayer = getCoreRelayer(targetChain, environment, targetChainProvider)
 
   const deliverys = coreRelayer.filters.Delivery(null, null, null, vaaHash)
@@ -263,7 +422,6 @@ async function pullEventsByVaaHash(
   )
 }
 */
-
 
 /*
 //TODO be able to find the VAA even if the sequence number rolls back
