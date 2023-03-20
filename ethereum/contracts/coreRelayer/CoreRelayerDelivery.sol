2a3a32abc3e137b7fc2b4ad6a8d71c425be8ea79
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-03-20 13:57:40 +0000

    removed console.sol

--- ethereum/contracts/coreRelayer/CoreRelayerDelivery.sol
+++ ethereum/contracts/coreRelayer/CoreRelayerDelivery.sol
@@ -7,7 +7,6 @@ import "../interfaces/IWormholeReceiver.sol";
 import "../interfaces/IDelivery.sol";
 import "./CoreRelayerGovernance.sol";
 import "./CoreRelayerStructs.sol";
-import "forge-std/console.sol";
 
 contract CoreRelayerDelivery is CoreRelayerGovernance {
     enum DeliveryStatus {
