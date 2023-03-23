1bbf6b3f36bb229989a0062a2ca1a23d08866675
Joe Howarth <josephehowarth@gmail.com>
2023-03-23 16:46:44 -0500

    Feat: relayer engine v2 support + vaa fetching via manifest

--- relayer-engine-v2/pkgs/sdk/src/structs.ts
+++ relayer-engine-v2/pkgs/sdk/src/structs.ts
@@ -10,7 +10,6 @@ export enum RelayerPayloadId {
 export interface DeliveryInstructionsContainer {
   payloadId: number // 1
   sufficientlyFunded: boolean
-  messages: MessageInfo[]
   instructions: DeliveryInstruction[]
   messages: MessageInfo[]
 }
@@ -40,22 +39,13 @@ export interface RedeliveryByTxHashInstruction {
   payloadId: number //2
   sourceChain: number
   sourceTxHash: Buffer
-  deliveryVAASequence: number
   sourceNonce: BigNumber
   targetChain: number
+  deliveryIndex: number
   multisendIndex: number
   newMaximumRefundTarget: BigNumber
   newReceiverValueTarget: BigNumber
   executionParameters: ExecutionParameters
-}
-
-export enum MessageInfoType {EMITTER_SEQUENCE, VAAHASH}
-
-export interface MessageInfo {
-  infoType: MessageInfoType,
-  emitterAddress: Buffer,
-  sequence: number,
-  vaaHash: Buffer
 }
 
 export function parsePayloadType(
@@ -84,38 +74,8 @@ export function parseDeliveryInstructionsContainer(
   const sufficientlyFunded = Boolean(bytes.readUInt8(idx))
   idx += 1
 
-  const numMessages = bytes.readUInt8(idx)
-  idx += 1
-
   const numInstructions = bytes.readUInt8(idx)
   idx += 1
-
-  let messages = [] as MessageInfo[]
-  for(let i=0; i < numMessages; ++i) {
-    const payloadId = bytes.readUint8(idx);
-    idx += 1;
-    const infoType = bytes.readUint8(idx) as MessageInfoType;
-    idx += 1;
-    let emitterAddress = Buffer.from([]);
-    let sequence = 0;
-    let vaaHash = Buffer.from([]);
-    if(infoType == MessageInfoType.EMITTER_SEQUENCE) {
-      emitterAddress = bytes.slice(idx, idx+32);
-      idx += 32;
-      sequence = ethers.BigNumber.from(Uint8Array.prototype.subarray.call(bytes, idx, idx+8)).toNumber();
-      idx += 8;
-    } else if(infoType == MessageInfoType.VAAHASH) {
-      vaaHash = bytes.slice(idx, idx + 32);
-      idx += 32;
-    }
-    messages.push({
-      infoType,
-      emitterAddress,
-      sequence,
-      vaaHash
-    })
-  }
-
   let instructions = [] as DeliveryInstruction[]
   for (let i = 0; i < numInstructions; ++i) {
     const targetChain = bytes.readUInt16BE(idx)
@@ -163,7 +123,6 @@ export function parseDeliveryInstructionsContainer(
   return {
     payloadId,
     sufficientlyFunded,
-    messages,
     instructions,
     messages,
   }
@@ -187,11 +146,14 @@ export function parseRedeliveryByTxHashInstruction(
   const sourceTxHash = bytes.slice(idx, idx + 32)
   idx += 32
 
-  const deliveryVAASequence = BigNumber.from(bytes.slice(idx, idx + 8))
-  idx += 8
+  const sourceNonce = BigNumber.from(bytes.slice(idx, idx + 4))
+  idx += 4
 
   const targetChain = bytes.readUInt16BE(idx)
   idx += 2
+
+  const deliveryIndex = bytes.readUint8(idx)
+  idx += 1
 
   const multisendIndex = bytes.readUint8(idx)
   idx += 1
@@ -209,8 +171,9 @@ export function parseRedeliveryByTxHashInstruction(
     payloadId,
     sourceChain,
     sourceTxHash,
-    deliveryVAASequence,
+    sourceNonce,
     targetChain,
+    deliveryIndex,
     multisendIndex,
     newMaximumRefundTarget,
     newReceiverValueTarget,
--- sdk/scripts/make_ethers_types.sh
+++ sdk/scripts/make_ethers_types.sh
@@ -5,7 +5,15 @@ SDK=$(dirname $0)/..
 DST=$SDK/src/ethers-contracts
 
 typechain --target=ethers-v5 --out-dir=$DST $SRC/*/*.json
+
+mkdir $SDK/../relayer_engine/pkgs
 mkdir $SDK/../relayer_engine/pkgs/sdk
 cp -r $SDK/src $SDK/../relayer_engine/pkgs/sdk
 cp $SDK/* $SDK/../relayer_engine/pkgs/sdk/
 cp $SDK/.gitignore $SDK/../relayer_engine/pkgs/sdk/.gitignore
+
+mkdir $SDK/../relayer-engine-v2/pkgs
+mkdir $SDK/../relayer-engine-v2/pkgs/sdk
+cp -r $SDK/src $SDK/../relayer-engine-v2/pkgs/sdk
+cp $SDK/* $SDK/../relayer-engine-v2/pkgs/sdk/
+cp $SDK/.gitignore $SDK/../relayer-engine-v2/pkgs/sdk/.gitignore
