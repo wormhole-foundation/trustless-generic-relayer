177e719d9d558bda9a9405e7e593ffd28e36cf60
chase-45 <chasemoran45@gmail.com>
2023-04-08 16:07:31 -0400

    added IRelayProvider comments and others

--- ethereum/contracts/interfaces/IWormholeReceiver.sol
+++ ethereum/contracts/interfaces/IWormholeReceiver.sol
@@ -3,13 +3,6 @@
 pragma solidity ^0.8.0;
 
 interface IWormholeReceiver {
-    struct DeliveryData {
-        bytes32 sourceAddress;
-        uint16 sourceChain;
-        uint256 maximumRefund;
-        bytes32 deliveryHash;
-        bytes payload;
-    }
 
     /**
      * @notice When a 'send' or 'forward' is performed with this contract as the target, this function will be invoked by the WormholeDelivery contract.
@@ -21,9 +14,11 @@ interface IWormholeReceiver {
      * refund and forwarding mechanisms from functioning.
      * 
      * msg.value for this call will be equal to the receiverValue specified in the send request.
+     *
+     * If this interface is improperly implemented, reverts, or exceeds the gasLimit (maxTransactionFee) specified by the send requester, this delivery will result in a ReceiverFailure.
      * 
      * @param deliveryData - This struct contains information about the delivery which is being performed.
-     * - sourceAddress - the (wormhole format) address on the sending chain which requested this delivery. Any contract / wallet is able to initiate a delivery anywhere by default.
+     * - sourceAddress - the (wormhole format) address on the sending chain which requested this delivery. Any address is able to initiate a delivery to anywhere else.
      * - sourceChain - the wormhole chain ID where this delivery was requested.
      * - maximumRefund - the maximum refund that can possibly be awarded at the end of this delivery, assuming no gas is used by receiveWormholeMessages.
      * - deliveryHash - the VAA hash of the deliveryVAA. If you do not want to potentially process this delivery multiple times, you should store this hash in state for replay protection.
@@ -34,4 +29,23 @@ interface IWormholeReceiver {
      * otherwise the VAA may be invalid or malicious.
      */
     function receiveWormholeMessages(DeliveryData memory deliveryData, bytes[] memory signedVaas) external payable;
+
+
+    /**
+     * @notice TargetDeliveryParameters is the struct that the relay provider passes into 'deliver'
+     * containing an array of the signed wormhole messages that are to be relayed
+     *
+     * @custom:member sourceAddress - the (wormhole format) address on the sending chain which requested this delivery. Any address is able to initiate a delivery to anywhere else.
+     * @custom:member sourceChain - the wormhole chain ID where this delivery was requested.
+     * @custom:member maximumRefund - the maximum refund that can possibly be awarded at the end of this delivery, assuming no gas is used by receiveWormholeMessages.
+     * @custom:member deliveryHash - the VAA hash of the deliveryVAA. If you do not want to potentially process this delivery multiple times, you should store this hash in state for replay protection.
+     * @custom:member payload - an optional arbitrary message which was included in the delivery by the requester.
+     */
+    struct DeliveryData {
+        bytes32 sourceAddress;
+        uint16 sourceChain;
+        uint256 maximumRefund;
+        bytes32 deliveryHash;
+        bytes payload;
+    }
 }
